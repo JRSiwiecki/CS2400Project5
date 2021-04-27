636808216cab345fb73d829531e71667d46e63c6
Joseph <46443751+TheMusket@users.noreply.github.com>
2021-04-27 12:19:17 -0700

    Base ArrayQueue implementation.

---
+++ ArrayQueue.java
@@ -0,0 +1,40 @@
+/**
+ * Queue implementation using arrays.
+ * @author Joseph
+ *
+ * @param <T>
+ */
+public class ArrayQueue<T> implements QueueInterface<T>
+{
+
+	@Override
+	public void enqueue(T newEntry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getFront() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
